a9ae8f8073cbaec356a61e8d472982b0baeec40d
Ivy Stephenson <istephensondev@gmail.com>
2021-02-12 22:04:39 -0600

    array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -5,21 +5,98 @@ const util = require('util');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    // Github Username:
+// Github Username:
 { type: 'input',
-  name: 'uername',
-  message: 'Enter your GitHub username.',
-
-}
-
+  name: 'username',
+  message: 'Enter your GitHub username:',
+validate: function (answer) {
+    if (answer.length<1) {
+        return console.log("You must enter a GitHub username.");
+    }
+    return true;
+  }
+},
+// GitHub Repo
+{
+    type: 'input',
+    name: 'repository',
+    message: "Enter the name of your GitHub Reopsitory:",
+    validate: function (answer) {
+        if (answer.length < 1)  {
+            return console.log("You must enter the name of your GitHub Repository.")
+        }
+        return true;
+    }
+},
+// Title of Project
+{
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+    validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("You must enter the title of your project.")
+        }
+    }
+},
+// Project Description
+{
+    type: 'input',
+    name:'description',
+    message:'Entere a description of your project:',
+    validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("You must enter a description of your project.")
+        }
+        return true;
+    }
+},
+// Project contribution
+{
+    type: 'input',
+    name:'contribution',
+    message:'Explain how users can contribute to your project (if necessary).',
+},
+// Project isntallation
+{
+    type: 'input',
+    name:'contribution',
+    message:'Explain how user would install for installation section (if necessary).', 
+},
+// Liscense
+{
+    type: 'list',
+    name: 'license',
+    message: 'Choose your license for your project.',
+    choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib'],
+    
+},
+// Usage of project
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Enter your project instructions and examples of project in use for Usage Section.',
+},
+// Test for Project
+{
+    type: 'input',
+    name: 'tests',
+    message: 'Provide tests for project, and explain how to test (if necessary).',
+},
 ];
+// // TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Your markdown file has been created.')
+    });
+}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // Function call to initialize app
+// init();
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
- 
